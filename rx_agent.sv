8c2b103cfacba702393b592c1e3c266bff1d2c88
venkateshwaraprashanth <40582958+venkateshwaraprashanth@users.noreply.github.com>
2019-06-09 20:21:05 -0700

    Rx monitor created and added to RX agent. Environment file has been created

--- rx_agent.sv
+++ rx_agent.sv
@@ -8,6 +8,8 @@ class rx_agent extends uvm_agent;
     
         rx_monitor       rx_mon;
 
+        uvm_analysis_port#(packet) agent_ap;
+
         function new (string name = "rx_agent", uvm_component parent=null);
             super.new (name, parent);
         endfunction
@@ -16,6 +18,11 @@ class rx_agent extends uvm_agent;
             super.build_phase(phase);
             rx_mon = rx_monitor::type_id::create("rx_mon", this);
         endfunction
+
+        virtual function void connect_phase(input uvm_phase phase);
+            super.connect_phase(phase);
+            this.agent_ap = rx_mon.mon_ap;
+        endfunction 
 
 endclass
 `endif
---
+++ rx_monitor.sv
@@ -0,0 +1,37 @@
+`ifndef	RX_MONITOR_SV
+`define	RX_MONITOR_SV
+
+class rx_monitor extends uvm_monitor;
+    `uvm_component_utils(rx_monitor)
+
+    virtual pkt_interface   pkt_vif;
+
+    uvm_analysis_port#(mon_pkt) mon_ap;
+
+    function new(input string name,input uvm_component parent);
+        super.new(name, parent);
+    endfunction: new
+
+    function void build_phase(uvm_phase phase);
+          super.build_phase(phase);
+          mon_ap = new("mon_ap", this);
+          uvm_config_db#(virtual pkt_interface)::get(this,"","pkt_vif",pkt_vif);
+          if(pkt_vif==null) begin
+            `uvm_error(get_name(), "Pkt interface not set")
+          end
+    endfunction
+
+    virtual task run_phase( input uvm_phase phase);
+        mon_pkt pkt_id = mon_pkt::typeid::create("pkt_id",this);
+        `uvm_info(get_name(), $sformatf("%m"), UVM_HIGH);
+        
+        forever begin
+            @(posedge pkt_vif.clk_156m25);
+            //drive signals over here
+
+            mon_ap.write(pkt_id);
+        end
+
+    endtask: run_phase
+
+endfunction: build_phase
