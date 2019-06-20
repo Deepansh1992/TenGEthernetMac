c289ba2043822ca82ac8002bc2bec89f3d5a503e
VLSIX410-Spring2019-5 <sf10075@VLSI04.ucsc-extension.edu>
2019-06-19 19:17:27 -0700

    no changes made

--- package.sv
+++ package.sv
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-package pck; 
-    `include *_sequence_item.sv
-    `include *_sequence.sv
-    `include *_driver.sv
-    `include *_monitor.sv
-    `include *_agent.sv
-    // `include scoreboard.sv
-    `include environment.sv
-    `include testclass.sv
-endpackage
-    
-=======
 package Macpackage
 	`include *_sequence_item.sv
 	`include *_sequence.sv
@@ -19,4 +6,3 @@ package Macpackage
 	`include monitor.sv
 	`include environment.sv
 endpackage
->>>>>>> 80672db5b19edcfcb0382a5c07c37fa926f1ecb7
