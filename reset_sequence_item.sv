d5b26ebdb19b400574687367066ea76c567bb512
Deepansh Agrawal <deepansh.agrawal@ssrlabs.com>
2019-10-22 11:22:44 -0700

    Removed unnecessary comments

--- reset_sequence_item.sv
+++ reset_sequence_item.sv
@@ -24,20 +24,6 @@ class reset_sequence_item extends uvm_sequence_item;
         `uvm_info(get_name(), $sformatf("%m"), UVM_MEDIUM);
     endfunction: new
 
-    //  Function: do_copy
-    // extern function void do_copy(uvm_object rhs);
-    //  Function: do_compare
-    // extern function bit do_compare(uvm_object rhs, uvm_comparer comparer);
-    //  Function: convert2string
-    // extern function string convert2string();
-    //  Function: do_print
-    // extern function void do_print(uvm_printer printer);
-    //  Function: do_record
-    // extern function void do_record(uvm_recorder recorder);
-    //  Function: do_pack
-    // extern function void do_pack();
-    //  Function: do_unpack
-    // extern function void do_unpack();
 
     
 endclass: reset_sequence_item
